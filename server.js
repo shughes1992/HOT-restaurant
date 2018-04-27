a2b89f6e99e939f728a87f4c431bb9c64f8d7469
Handrew <andrew.a.han@gmail.com>
2018-04-26 21:02:02 -0400

    more changes

--- server.js
+++ server.js
@@ -22,13 +22,11 @@ app.get("/", function(req, res) {
   }); 
 
 app.get("/api/tables", function(req, res) {
-    // res.sendFile(path.join(__dirname, "tables.html"));
     res.send(tables);
   });
 
 app.get("/tables", function(req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
-    // res.send(tables);
   });
 
 app.get("/reserve", function(req, res) {
